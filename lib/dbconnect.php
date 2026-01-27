0c6d52fc5c7e0095c5b3ddd6bf09f4cc8518d3d5
Maria Konstantinidou <91637547+mariakon261@users.noreply.github.com>
2026-01-27 14:11:01 +0200

    Update dbconnect.php

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,19 +1,17 @@
 <?php
 
+
 $host     = 'localhost'; 
-$username = "iee2021075";
-//$username = 'root';       
+$username = "iee2021075";       
 $password = "1234";
-//$password = '';
 $dbname   = "kseri_db"; 
 $socket   = "/home/student/iee/2021/iee2021075/mysql/run/mysql.sock";
 
 
 $mysqli = new mysqli($host, $username, $password, $dbname, null, $socket);
 
-//$mysqli = new mysqli($host, $username, $password, $dbname);
-
 if ($mysqli->connect_errno) {
     die("Connection failed: " . $mysqli->connect_error);
 }
+
 ?>
